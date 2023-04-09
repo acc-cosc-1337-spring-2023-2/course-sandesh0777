a5e7ad75958f0ffef14e27aeed0fa6841f49e927
sandesh0777 <123131974+sandesh0777@users.noreply.github.com>
2023-04-09 03:55:01 +0000

    Assignment done

--- src/homework/08_memory/main.cpp
+++ src/homework/08_memory/main.cpp
@@ -1,5 +1,59 @@
+#include "tic_tac_toe.h"
+# include "memory.h"
+#include <iostream>
+
+using namespace std;
+
 int main()
 {
+    TicTacToeManager manager;
+    string again;
+    int x, o, t;
+
+    do
+    {
+        unique_ptr<TicTacToe> game;
+        string player = "X";
+
+        while (!game || game->game_over() == false)
+        {
+            cout << "\n\n" << player << " Turn\n";
+            cout << *game;
+
+            int position;
+            cout << "\nEnter position from 1 to 9: ";
+            cin >> position;
+
+            try
+            {
+                game->mark_board(position);
+                cout << "\n\n";
+                cout << *game;
+
+                player = player == "X" ? "O" : "X";
+            }
+            catch (const string& error)
+            {
+                cout << error << endl;
+            }
+        }
+
+        cout << "Game Over\n";
+        cout << *game;
+
+        manager.save_game(*game);
+        cout << manager << endl;
+
+        cout << "Play Again? (Y/N): ";
+        cin >> again;
+    } while (again == "Y" || again == "y");
+
+    manager.get_winner_total(o, x, t);
+    cout << "\n\nFinal Score:\n";
+    cout << "X wins: " << x << endl;
+    cout << "O wins: " << o << endl;
+    cout << "Ties: " << t << endl;
+    cout << manager;
 
     return 0;
 }
--- test/homework/08_memory/08_memory_tests.cpp
+++ test/homework/08_memory/08_memory_tests.cpp
@@ -1,7 +1,54 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+# include "tic_tac_toe.h"
+# include "memory.h"
 
-TEST_CASE("Verify Test Configuration", "verification") {
-	REQUIRE(true == true);
+TEST_CASE("Test TicTacToe class", "[TicTacToe]")
+{
+    TicTacToe game;
+    game.start_game("X");
+    REQUIRE(game.get_player() == "X");
+    game.mark_board(1);
+    REQUIRE(game.get_player() == "O");
+    REQUIRE(game.get_winner() == "");
+    game.mark_board(2);
+    REQUIRE(game.get_player() == "X");
+    game.mark_board(3);
+    REQUIRE(game.get_player() == "O");
+    game.mark_board(4);
+    REQUIRE(game.get_player() == "X");
+    game.mark_board(5);
+    REQUIRE(game.get_player() == "O");
+    game.mark_board(7);
+    REQUIRE(game.get_winner() == "O");
+    REQUIRE(game.game_over() == true);
 }
 
+TEST_CASE("Test TicTacToeManager class", "[TicTacToeManager]")
+{
+    TicTacToeManager manager;
+    TicTacToe game1;
+    game1.start_game("X");
+    game1.mark_board(1);
+    game1.mark_board(2);
+    game1.mark_board(3);
+    manager.save_game(game1);
+    TicTacToe game2;
+    game2.start_game("O");
+    game2.mark_board(1);
+    game2.mark_board(2);
+    game2.mark_board(4);
+    game2.mark_board(5);
+    manager.save_game(game2);
+    TicTacToe game3;
+    game3.start_game("X");
+    game3.mark_board(1);
+    game3.mark_board(4);
+    game3.mark_board(7);
+    manager.save_game(game3);
+    int x_win = 0, o_win = 0, ties = 0;
+    manager.get_winner_total(o_win, x_win, ties);
+    REQUIRE(x_win == 2);
+    REQUIRE(o_win == 1);
+    REQUIRE(ties == 0);
+}
